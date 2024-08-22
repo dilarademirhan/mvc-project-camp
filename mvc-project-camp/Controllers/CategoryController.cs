1c593c5488305786d56f788f06af7bcdef501417
dilarademirhan <dilara.demirhan@stu.fsm.edu.tr>
2024-08-22 03:03:50 +0300

    Category operations

---
+++ mvc-project-camp/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace mvc_project_camp.Controllers
+{
+    public class CategoryController : Controller
+    {
+        CategoryManager cm = new CategoryManager(); 
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult GetCategoryList()
+        {
+            var categoryvalues = cm.GetAllBL();
+            return View(categoryvalues);
+        }
+    }
+}

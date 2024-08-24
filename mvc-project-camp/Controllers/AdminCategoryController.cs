a767178a8c4edba9159af4e3627d00df61bbd6b3
dilarademirhan <dilara.demirhan@stu.fsm.edu.tr>
2024-08-24 03:43:39 +0300

    Theme operations

---
+++ mvc-project-camp/Controllers/AdminCategoryController.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace mvc_project_camp.Controllers
+{
+	public class AdminCategoryController : Controller
+	{
+		CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+		public IActionResult Index()
+		{
+			var categoryValues = categoryManager.GetList();
+			return View(categoryValues);
+		}
+	}
+}

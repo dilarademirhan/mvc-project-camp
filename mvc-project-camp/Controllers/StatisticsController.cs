408eb9212c054fba9c06e833cd052d4421371765
dilarademirhan <dilara.demirhan@stu.fsm.edu.tr>
2024-08-26 19:01:46 +0300

    statistics

---
+++ mvc-project-camp/Controllers/StatisticsController.cs
@@ -0,0 +1,22 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace mvc_project_camp.Controllers
+{
+    public class StatisticsController : Controller
+    {
+        CategoryManager cm = new CategoryManager(new EfCategoryDal());
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult GetCategoriesNumber()
+        {
+            int total = cm.GetList().Count();
+            return View(total);
+        }
+
+    }
+}
